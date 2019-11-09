7a0a60fe77d459c61f365d1b0268b75de8a2008c
Kacper1999 <57040960+Kacper1999@users.noreply.github.com>
2019-11-09 21:07:35 +0100

    added visualization

--- jarvis_algorithm.py
+++ jarvis_algorithm.py
@@ -1,7 +1,6 @@
 import generating_random_points as rand_p
-import time_functions as time_f
-import qualify_by_angle as qualify_by_a
-import ploting_points as plot_p
+from time_functions import time_func
+from qualify_by_angle import get_key
 
 
 def jarvis(points, error=10 ** (-8)):
@@ -13,7 +12,7 @@ def jarvis(points, error=10 ** (-8)):
     stack = [p0]
     # I add append another point to stack so that I can append "p0" to "copied_points" and write a simple while loop,
     # if I were not to do that I wouldn't be able to write simple condition like "stack[0] != stack[-1].
-    key = qualify_by_a.get_key(stack[-1], error)
+    key = get_key(stack[-1], error)
     p = min(copied_points, key=key)
     # I append p so that I will know when I created a hull convex
     stack.append(p)
@@ -21,7 +20,7 @@ def jarvis(points, error=10 ** (-8)):
     copied_points.append(p0)
 
     while stack[0] != stack[-1]:
-        key = qualify_by_a.get_key(stack[-1], error)
+        key = get_key(stack[-1], error)
         p = min(copied_points, key=key)
         copied_points.remove(p)
         stack.append(p)
@@ -30,10 +29,7 @@ def jarvis(points, error=10 ** (-8)):
 
 def main():
     random_points = rand_p.get_rand_points(10 ** 3)
-    plot_p.plot_points(random_points)
-    result = jarvis(random_points)
-    plot_p.plot_points(result)
-    time_f.time_func(jarvis, random_points)
+    time_func(jarvis, random_points)
 
 
 if __name__ == "__main__":
